894de7319a2e78c3521480409c987c7501822351
akhegade <anilhegde006@gmail.com>
2023-09-20 17:49:15 +0530

    refactoring the code

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 let timer = 60;
 let score = 0;
 let hitVal = 0;
-function updateTimerVal(val) {
-  document.querySelector("#timeVal").textContent = val;
+
+/*@function-name:updateElementContent
+  @params: val : values to be deplpayed 
+           eleId : element id in which val should painted
+ @return : not retur anything
+*/
+function updateElementContent(val, eleId) {
+  document.querySelector(eleId).textContent = val;
 }
 
-function updateScoreVal(val) {
-  document.querySelector("#scoreVal").textContent = val;
-}
-
+/*@function-name:makeBubble
+  @desc : funtion will created new element with specified time
+  @params: not required 
+           
+ @return : not return anything
+*/
 function makeBubble() {
   let clutter = "";
-  for (let i = 1; i <= 170; i++) {
+  for (let i = 1; i <= 168; i++) {
     clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
   }
   document.querySelector("#pbtm").innerHTML = clutter;
 }
 
+/*
+@function-name:startTimer
+@desc: function will add timer to page, on timer done , will clear interval and add new elements to pbtm tag
+*/
 function startTimer() {
   let timerInv = setInterval(() => {
     if (timer > 0) {
       timer--;
-      updateTimerVal(timer);
+      updateElementContent(timer, "#timeVal");
     } else {
       clearInterval(timerInv);
       document.querySelector(
@@ -31,16 +43,30 @@ function startTimer() {
   }, 1000);
 }
 
+/*
+@function-name:makeHit
+@desc: function will create random number and it to hit box 
+*/
 function makeHit() {
   hitVal = Math.floor(Math.random() * 10);
-  document.querySelector("#hitVal").textContent = hitVal;
+  updateElementContent(hitVal, "#hitVal");
 }
 
+/*
+@function-name:increaseScore
+@desc: function will compute the score and it will add score to score section 
+*/
 function increaseScore() {
   score += 10;
-  document.querySelector("#scoreVal").textContent = score;
+  updateElementContent(score, "#scoreVal");
 }
 
+/*
+adding event to pbtm element , when element within the pbtm tab clicked ,
+function will check wether hitVal and clicked value is same ,
+if same it will call the increasScore , makeBubble and makeHit function
+else , it will updat backgound color of element into red, which means both are not same
+*/
 document.querySelector("#pbtm").addEventListener("click", function (evnt) {
   console.log("(evnt.target):", evnt);
 
@@ -56,17 +82,21 @@ document.querySelector("#pbtm").addEventListener("click", function (evnt) {
   }
 });
 
+/*
+@function-name:onReStart
+@desc: function will reset all value to default value 
+*/
 function onReStart() {
   timer = 60;
   score = 0;
-  updateTimerVal(timer);
+  updateElementContent(timer, "#timeVal");
   makeBubble();
   makeHit();
   startTimer();
-  updateScoreVal(score);
+  updateElementContent(score, "#scoreVal");
 }
 
-updateTimerVal(timer);
+updateElementContent(timer, "#timeVal");
 makeBubble();
 startTimer();
 makeHit();
